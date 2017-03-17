c57a9af59a09379c3d0642680a8781632344ec16
Charlotte Gisele Weil <weilcharlotte@gmail.com>
2017-03-16 22:42:32 -0700

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# chabby_portfolio
---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+var smartscan = ["The Breitling B55 is a swiss-made connected chrongraph. I coded the iOS companion application of this watch and designed the bluetooth specification of the interactions between the watch and the smartphone."]
+
+btn_smartscan.on("mouseover", function(d, i){$("#project_text").html(smartscan);
+            })
+            .on("mouseout", function(){$("#project_text").html(" ");});
