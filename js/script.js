bbb5fe54bf3903a2106c4649cf5e22ec41421185
Charlotte Gisele Weil <weilcharlotte@gmail.com>
2017-03-30 10:03:56 -0700

    des trucs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
+$(function(){
+	main();
+})
 
-var smartscan = ["The Breitling B55 is a swiss-made connected chrongraph. I coded the iOS companion application of this watch and designed the bluetooth specification of the interactions between the watch and the smartphone."]
+function main(){
+	var smartscan_infos = ["Smart Scan", "images/smartscan.png","The Breitling B55 is a swiss-made connected chrongraph. I coded the iOS companion application of this watch and designed the bluetooth specification of the interactions between the watch and the smartphone."]
 
-btn_smartscan.on("mouseover", function(d, i){$("#project_text").html(smartscan);
-            })
-            .on("mouseout", function(){$("#project_text").html(" ");});
+	
+		detailsOnHover('#btn_smartscan', smartscan_infos);
+}
+
+function detailsOnHover(btnID, project_infos){
+		var btn = $(btnID)
+
+		btn.on("mouseover", function(d, i){ 
+			$("#project_img").attr("src",project_infos[1])
+			$("#project_title").html(project_infos[0]);
+			$("#project_text").html(project_infos[2]); 
+		}).on("mouseout", function(){
+			$("#project_text").html(" ");
+			$("#project_img").attr("src"," ")
+			$("#project_title").html(" ");
+		});
+}
+
