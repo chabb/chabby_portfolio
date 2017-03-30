877fcbd8123241e5adc85c8548fbee11ba660305
Francois Chabbey <francois.chabbey@thoughtspot.com>
2017-03-30 11:00:28 -0700

    Introduces dynamic generation and behaviour by leveraging automic production of tags

--- js/script.js
+++ js/script.js
@@ -3,19 +3,47 @@ $(function(){
 })
 
 function main(){
-	var smartscan_infos = ["Smart Scan", "images/smartscan.png","The Breitling B55 is a swiss-made connected chrongraph. I coded the iOS companion application of this watch and designed the bluetooth specification of the interactions between the watch and the smartphone."]
-
+	let current_infos = [{
+		img: 'images/smartscan.png',
+		title: 'SmartScan', 
+		description: "The Breitling B55 is a swiss-made connected chrongraph. I coded the iOS companion application of this watch and designed the bluetooth" 
+			+ "specification of the interactions between the watch and the smartphone.",
+		id: 'smartScan',
+		link: 'http://www.smartscan.icare.ch',
+	}];
 	
-		detailsOnHover('#btn_smartscan', smartscan_infos);
+	let older_infos = [];
+
+		//detailsOnHover('#btn_smartscan', smartscan_infos);
+
+	current_infos.forEach((info) => {
+		detailsOnHover('.current-project', info);
+	});	
 }
 
-function detailsOnHover(btnID, project_infos){
-		var btn = $(btnID)
+function detailsOnHover(containerId, project_infos){
+		let ctn = $(containerId);
+		let link = project_infos.link;
+		let img = project_infos.img;
+		let title = project_infos.title;
+		let description = project_infos.description;
+		let btnId = project_infos.id;
+		
+
+
+
+		let html = `<a target="_blank" href="${link}"> 
+			   			<div id="${btnId}" class="btn waves-light">${title}</div>
+			   		</a>`
+		// generate content
+		ctn.append(html);
+
+		btn = $('#' + btnId);
 
 		btn.on("mouseover", function(d, i){ 
-			$("#project_img").attr("src",project_infos[1])
-			$("#project_title").html(project_infos[0]);
-			$("#project_text").html(project_infos[2]); 
+			$("#project_img").attr("src", img)
+			$("#project_title").html(title);
+			$("#project_text").html(description); 
 		}).on("mouseout", function(){
 			$("#project_text").html(" ");
 			$("#project_img").attr("src"," ")
